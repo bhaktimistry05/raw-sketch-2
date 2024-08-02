90fc64491b69b07a765d71c8fd62e933d06fa887
bhaktimistry05 <bhakti.mistry.sod23@anu.edu.in>
2024-08-02 02:24:55 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 640);
+  background(224, 200, 45);
+  circle(150, 150, 100)
+}
+
+function draw(){
+}
+
+function mouseDragged(){
+  fill(121, 65, 145);
+  noStroke();
+  line(100/2, 100/2, 50, 50);
+  
+}
